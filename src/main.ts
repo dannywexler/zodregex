3f03a7f3344ae31a55fdc89f4fdebc8eda787535
Danny Wexler <dannywexler@gmail.com>
2024-12-15 09:35:50 -0800

    add wireit to devDependencies

--- src/main.ts
+++ src/main.ts
@@ -1,31 +1,31 @@
 import type { z } from "zod";
 
 export function zodRegex<
-    GroupSchema extends z.ZodType<Record<string, unknown>>,
+	GroupSchema extends z.ZodType<Record<string, unknown>>,
 >(groupSchema: GroupSchema, regex: RegExp, ...alternateRegexes: Array<RegExp>) {
-    const allRegexes = [regex, ...alternateRegexes];
-    return (testString: string) => {
-        let match: RegExpMatchArray | null = null;
-        for (const reg of allRegexes) {
-            match = testString.match(reg);
-            if (match) {
-                break;
-            }
-        }
-        if (!match) {
-            return null;
-        }
-        const { groups } = match;
-        if (!groups) {
-            return null;
-        }
-        const res = groupSchema.safeParse(groups) as z.SafeParseReturnType<
-            z.input<GroupSchema>,
-            z.output<GroupSchema>
-        >;
-        if (!res.success) {
-            return null;
-        }
-        return res.data;
-    };
+	const allRegexes = [regex, ...alternateRegexes];
+	return (testString: string) => {
+		let match: RegExpMatchArray | null = null;
+		for (const reg of allRegexes) {
+			match = testString.match(reg);
+			if (match) {
+				break;
+			}
+		}
+		if (!match) {
+			return null;
+		}
+		const { groups } = match;
+		if (!groups) {
+			return null;
+		}
+		const res = groupSchema.safeParse(groups) as z.SafeParseReturnType<
+			z.input<GroupSchema>,
+			z.output<GroupSchema>
+		>;
+		if (!res.success) {
+			return null;
+		}
+		return res.data;
+	};
 }
